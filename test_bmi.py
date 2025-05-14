f39e31148586273dd6e99c2f1a844e9e9d13491b
Ei Than Thar Aung <thanthar.24@ichat.sp.edu.sg>
2025-05-14 16:00:04 +0800

    Created test_bmi.py

---
+++ test_bmi.py
@@ -0,0 +1,20 @@
+import Lab2.bmi as BMI
+
+def test_bmi_over_weight():
+    expected_result=1
+    weight=97
+    test_result=BMI.calculate_bmi (1.73,weight)
+    assert (test_result==expected_result)
+
+def test_bmi_under_weight():
+    expected_result=-1
+    weight=37
+    test_result=BMI.calculate_bmi (1.73,weight)
+    assert (test_result==expected_result)
+
+
+def test_bmi_normal_weight():
+    expected_result=0
+    weight=57
+    test_result=BMI.calculate_bmi (1.73,weight)
+    assert (test_result==expected_result)
